77c259419b67287a2702be0461b3d19fa1cda083
emonShimoul <emonshimoul@gmail.com>
2023-01-07 19:45:26 +0600

    completed counter functionality using redux-toolkit

--- src/features/counter/CounterView.js
+++ src/features/counter/CounterView.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increaseByAmount, increment, reset } from './counterSlice';
 
 const CounterView = () => {
+    const count = useSelector(state => state.counter.count);
+    // console.log(count);
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <h2>CounterView</h2>
+            <h2>Count: {count}</h2>
+            <button onClick={() => { dispatch(increment()) }}>Increment</button>
+            <button onClick={() => { dispatch(decrement()) }}>Decrement</button>
+            <button onClick={() => { dispatch(reset()) }}>Reset</button>
+            <button onClick={() => { dispatch(increaseByAmount(5)) }}>Increase by 5</button>
         </div>
     );
 };
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -8,14 +8,17 @@ const counterSlice = createSlice({
             state.count = state.count + 1;
         },
         decrement: state => {
-            state.count = state.count + 1;
+            state.count = state.count - 1;
         },
         reset: state => {
             state.count = 0;
         },
+        increaseByAmount: (state, action) => {
+            state.count = state.count + action.payload;
+        },
     },
 });
 
-export const { increment, decrement, reset } = counterSlice.actions;
+export const { increment, decrement, reset, increaseByAmount } = counterSlice.actions;
 
 export default counterSlice.reducer;
